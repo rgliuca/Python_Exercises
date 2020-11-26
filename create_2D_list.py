5d3c098971bac30c53906bf4e7636f64cea64538
rgliuca <44307228+rgliuca@users.noreply.github.com>
2020-11-26 07:49:24 -0800

    Update create_2D_list.py

--- create_2D_list.py
+++ create_2D_list.py
@@ -1,3 +1,8 @@
 def create_2D_list(num_rows, num_cols):
     return [[num_cols*row + col for col in range(num_cols)] for row in
             range(num_rows)]
+
+
+matrix = create_2D_list(10, 10)
+for each_row in matrix:
+    print(each_row)
